95634d5a98db0eb2ec08d3bf5747b5cc41793db3
SonikaMadheswaran <sonikamadheswaran@gmail.com>
2025-08-24 21:05:13 +0530

    Fix: updated server logic and modified Home component

--- Server/index.js
+++ Server/index.js
@@ -1,35 +1,54 @@
-const express =require('express')
-const mongoose =require('mongoose')
-const cors=require('cors')
-const TodoModel=require('./Models/Todo')
-const app =express()
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const TodoModel = require('./Models/Todo')
+
+const app = express()
 app.use(cors())
 app.use(express.json())
+
 mongoose.connect('mongodb://127.0.0.1:27017/test')
-app.get('/get',(req,res)=>{
+
+app.get('/get', (req, res) => {
     TodoModel.find({})
-    .then(result=>res.json(result))
-    .catch(err=>res.json(err))
+        .then(result => res.json(result))
+        .catch(err => res.json(err))
 })
-app.put('/update/:id',(req,res)=>{
-    const {id}=req.params;
+
+app.put('/update/:id', (req, res) => {
+    const { id } = req.params
     TodoModel.findByIdAndUpdate(id, { done: true }, { new: true })
-    .then(result=>res.json(result))
-    .catch(err=>res.json(err))
+        .then(result => res.json(result))
+        .catch(err => res.json(err))
 })
-app.delete('/delete/:id',(req,res)=>{
-    const {id}=req.params;
-    TodoModel.findByIdAndDelete({_id:id})
-    .then(result=>res.json(result))
-    .catch(err=>res.json(err))
+
+app.put('/update_name/:id', (req, res) => {
+    const { id } = req.params
+    const { task } = req.body  
+
+    TodoModel.findByIdAndUpdate(
+        id,
+        { task: task },  
+        { new: true }
+    )
+        .then(result => res.json(result))
+        .catch(err => res.json(err))
 })
-app.post('/add',(req,res)=>{
-    const task=req.body.task;
-    TodoModel.create({
-        task:task
-    }).then(result=>res.json(result))
-    .catch(err=>res.json(err))
+
+app.delete('/delete/:id', (req, res) => {
+    const { id } = req.params
+    TodoModel.findByIdAndDelete(id)
+        .then(result => res.json(result))
+        .catch(err => res.json(err))
 })
-app.listen(3001,()=>{
-    console.log("Server is Running")
+
+app.post('/add', (req, res) => {
+    const { task } = req.body
+    TodoModel.create({ task: task })
+        .then(result => res.json(result))
+        .catch(err => res.json(err))
+})
+
+app.listen(3001, () => {
+    console.log("✅ Server is Running on port 3001")
 })
--- todolist/src/Home.jsx
+++ todolist/src/Home.jsx
@@ -1,56 +1,103 @@
-import React,{ useEffect,useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Create from './Create'
 import axios from 'axios'
-import { BsCircleFill, BsFillTrashFill, BsFillCheckCircleFill } from 'react-icons/bs';
+import { BsCircleFill, BsFillTrashFill, BsFillCheckCircleFill, BsFillPencilFill } from 'react-icons/bs';
+
 function Home() {
-    const [todos,setTodos]=useState([])
-    useEffect(()=>{
+    const [todos, setTodos] = useState([])
+    const [editingId, setEditingId] = useState(null)
+    const [editText, setEditText] = useState("")
+
+    useEffect(() => {
         axios.get('http://localhost:3001/get')
-        .then(result=>setTodos(result.data))
-        .catch(err=>console.log(err))
-    },[])
-    const handleEdit=(id)=>{
+            .then(result => setTodos(result.data))
+            .catch(err => console.log(err))
+    }, [])
+
+    const handleToggleDone = (id) => {
         axios.put(`http://localhost:3001/update/${id}`)
-        .then(result=>{
-            console.log(result)
-            location.reload()
-        })
-        .catch(err=>console.log(err))
+            .then(() => {
+                setTodos(prev =>
+                    prev.map(todo =>
+                        todo._id === id ? { ...todo, done: !todo.done } : todo
+                    )
+                )
+            })
+            .catch(err => console.log(err))
     }
-    const handleDelete=(id)=>{
+
+    const handleDelete = (id) => {
         axios.delete(`http://localhost:3001/delete/${id}`)
-        .then(result=>{
-            console.log(result)
-            location.reload()
-        })
-        .catch(err=>console.log(err))
+            .then(() => {
+                setTodos(prev => prev.filter(todo => todo._id !== id))
+            })
+            .catch(err => console.log(err))
     }
-  return (
-    <div>
-        <h2>Todo List</h2>
-        <Create />
-        {
-            todos.length===0 
-            ?
-            <div><h2>No Record</h2></div>
-            :
-            todos.map(todo=>(
-                <div className='task'>
-                    <div className='checkbox' onClick={()=>handleEdit(todo._id)}>
-                        {todo.done ? 
-                            <BsFillCheckCircleFill className='icon'></BsFillCheckCircleFill>
-                        :   <BsCircleFill className='icon'/>
-                        }
-                        <p className={todo.done ? "line_through" : ""}>{todo.task}</p>
-                    </div>
-                    <div>
-                        <span><BsFillTrashFill className='icon' onClick={()=>handleDelete(todo._id)}/></span>
-                    </div>
-                </div>
-            ))
-        }
-    </div>
-  )
+
+    const handleEditClick = (id, currentTask) => {
+        setEditingId(id)
+        setEditText(currentTask)
+    }
+
+    const handleUpdateTask = (id) => {
+        axios.put(`http://localhost:3001/update_name/${id}`, { task: editText }) // ✅ match backend
+            .then(() => {
+                setTodos(prev =>
+                    prev.map(todo =>
+                        todo._id === id ? { ...todo, task: editText } : todo
+                    )
+                )
+                setEditingId(null)
+                setEditText("")
+            })
+            .catch(err => console.log(err))
+    }
+
+    return (
+        <div>
+            <h2>Todo List</h2>
+            <Create />
+            {
+                todos.length === 0
+                    ? <div><h2>No Record</h2></div>
+                    : todos.map(todo => (
+                        <div className='task' key={todo._id}>
+                            <div className='checkbox' onClick={() => handleToggleDone(todo._id)}>
+                                {todo.done
+                                    ? <BsFillCheckCircleFill className='icon' />
+                                    : <BsCircleFill className='icon' />
+                                }
+                                {editingId === todo._id ? (
+                                    <input
+                                        type="text"
+                                        value={editText}
+                                        onChange={(e) => setEditText(e.target.value)}
+                                        onBlur={() => handleUpdateTask(todo._id)} // ✅ save on blur
+                                        autoFocus
+                                    />
+                                ) : (
+                                    <p className={todo.done ? "line_through" : ""}>{todo.task}</p>
+                                )}
+                            </div>
+                            <div>
+                                <span>
+                                    <BsFillPencilFill
+                                        className='icon'
+                                        onClick={() => handleEditClick(todo._id, todo.task)}
+                                    />
+                                </span>
+                                <span>
+                                    <BsFillTrashFill
+                                        className='icon'
+                                        onClick={() => handleDelete(todo._id)}
+                                    />
+                                </span>
+                            </div>
+                        </div>
+                    ))
+            }
+        </div>
+    )
 }
 
 export default Home
